43b44ba797ad246b52973ef0cfa6477e5ac2ef69
Wang Liang <boende@qq.com>
2023-05-10 09:40:56 +0800

    js

--- cafebabe-web/src/main/java/app/cafebabe/web/config/CorsConfig.java
+++ cafebabe-web/src/main/java/app/cafebabe/web/config/CorsConfig.java
@@ -1,15 +1,25 @@
 package app.cafebabe.web.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
-public class CorsConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**");
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                "http://127.0.0.1:5173",
+                                "https://cafebabe.app"
+                        )
+                        .allowedMethods("*");
+            }
+        };
     }
 }
--- cafebabe-web/src/main/java/app/cafebabe/web/controller/api/ShareFileController.java
+++ cafebabe-web/src/main/java/app/cafebabe/web/controller/api/ShareFileController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
+
 @RestController
 @RequestMapping("/api/share")
 public class ShareFileController {
